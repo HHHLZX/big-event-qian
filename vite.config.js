f7856a35c004a273cb9cb12a7e243ae9b0352bed
HHHLZX <138979515+HHHLZX@users.noreply.github.com>
2024-10-30 20:25:50 +0800

    配置ghpages

--- vite.config.js
+++ vite.config.js
@@ -5,25 +5,26 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [
+        vue(),
+    ],
+    base: '/big-event-qian/',
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    //配置代理
+    server: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8080', // 后端服务器地址
+                changeOrigin: true, // 是否改变请求域名
+                rewrite: (path) => path.replace(/^\/api/, '')
+                //将原有请求路径中的api替换为''
+            }
+        }
     }
-  },
-  //配置代理
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080', // 后端服务器地址
-        changeOrigin: true, // 是否改变请求域名
-        rewrite: (path) => path.replace(/^\/api/, '')
-        //将原有请求路径中的api替换为''
-      }
-    }
-  }
 })
 
 
